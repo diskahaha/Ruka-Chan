0df5f33e29d04e3dc071e54f793d0e087c2de880
Amirr <107915452+USER0106@users.noreply.github.com>
2022-08-03 11:34:06 +0800

    Update info-bioowner.js

--- plugins/info-bioowner.js
+++ plugins/info-bioowner.js
@@ -1,24 +1,24 @@
 let handler = async (m, { conn }) => {
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `*───────[ BIODATA OWNER ]───────*
-*💌 Nama* : ᴅᴀɴɪ-ʙᴏᴛᴢ
-*✉️ Nama RL* : ᴅᴀɴɪ ᴏғғᴄɪᴀʟ
+*💌 Nama* : Amirr
+*✉️ Nama RL* : Amirr
 *♂️ Gender* : Laki - laki
 *🕋 Agama* : Islam
-*⏰ Tanggal lahir* : 16 Desember 2007
+*⏰ Tanggal lahir* : 01, juni, 2007
 *🎨 Umur* : 15
 *🧮 Kelas* : 9
 *🧩 Hobby* : Nonton Hanime, Chatting, Recode script bot
 *💬 Sifat* : Idiot, Tidak Ramah, Kanjut, Prik, Pedofil
-*🗺️ Tinggal* : Indo, Kalsel, Kandangan, Hss
+*🗺️ Tinggal* : Indo, Kaltim, Muara Badak, Hss
 *❤️ Suka* : warnah kuning & biru, Hanime, waifu, loli, trap, furry, kucing
 *💔 Benci* : autis, anak epep, seleb
 
 *───────[ SOSIAL MEDIA ]───────*
-*📷 instagran* : @Dani_Offcial
-*🇫  Facebook* : Dani_Offc
-*🏮 Chanel Youtube* : DaniBotz
-*🐈 Github:* YTDANIBOTZ
+*📷 instagran* : @amirrwibuu_01
+*🇫  Facebook* : Amirr
+*🏮 Chanel Youtube* : GK PUNYA
+*🐈 Github:* USER0106
 
 `
 conn.sendHydrated(m.chat, teksbio, wm, ppown, "wa.me/" + nomorown, "💬 ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], m)
