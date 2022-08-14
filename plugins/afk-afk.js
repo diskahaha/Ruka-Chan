34cee79d77049393827abff5146154592a69da9c
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-08-14 15:48:04 +0700

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -1,26 +1,16 @@
-import fs from 'fs'
-import fetch from 'node-fetch'
-let handler = async (m, { text }) => {
-let name = m.pushName || conn.getName(m.sender)
-
-let user = global.db.data.users[m.sender]
-let wibu = `https://api.zacros.my.id/randomimg/loli`    
-let thumb = await(await fetch(wibu)).buffer()
-user.afk = + new Date
-user.afkReason = text
- conn.sendButtonDoc(m.chat, `${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}`, wm, 'Jangan Ganggu Ya', 'Bilek', m,  { contextInfo: { externalAdReply: { showAdAttribution: true,
-        mediaUrl: "https://Instagram.com/bot_whangsaf",
-        mediaType: "VIDEO",
-        description: "https://Instagram.com/bot_whangsaf", 
-        title: 'Simple Bot Esm',
-        body: wm,
-        thumbnail: thumb,
-        sourceUrl: sgc
-    }
-    } })
-            }
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    let user = global.db.data.users[m.sender]
+    user.afk = + new Date
+    user.afkReason = text
+    let caption = `*${conn.getName(m.sender)}* Sekarang lagi AFK${text ? ': ' + text : ''}`
+    let kataafk = ['mau turu', 'mau nyolong', 'Ke rumah ayang', 'jagain lilin', 'beli pop es', 'kawin lari', 'main kelereng', 'petak umpet', 'push renk', 'push up joni', 'olahraga', 'onani', 'beraq', 'open bo', 'di suruh emak', 'kerja']
+conn.sendButton(m.chat, caption, author, null, [
+        ['Ikut Afk', '.afk ' + kataafk.getRandom()]
+    ], m)
+}
 handler.help = ['afk [alasan]']
 handler.tags = ['main']
 handler.command = /^afk$/i
 
 export default handler
+//tes
