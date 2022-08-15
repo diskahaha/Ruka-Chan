9a506e8a29f1f27e53ffcae6cf96e0e4642f295a
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-08-15 17:07:17 +0700

    Update toimg.js

--- plugins/toimg.js
+++ plugins/toimg.js
@@ -1,17 +1,30 @@
-let { webp2png } = require('../lib/webp2mp4')
-let handler = async (m, { conn, usedPrefix, command }) => {
-  if (!m.quoted) throw `balas stiker dengan perintah *${usedPrefix + command}*`
-  let mime = m.quoted.mimetype || ''
-  if (!/webp/.test(mime)) throw `balas stiker dengan perintah *${usedPrefix + command}*`
-  let media = await m.quoted.download()
-  let out = Buffer.alloc(0)
-  if (/webp/.test(mime)) {
-    out = await webp2png(media)
-  }
-  await conn.sendFile(m.chat, out, 'out.png', '', m, false, { thumbnail: out })
-}
-handler.help = ['toimg']
-handler.tags = ['sticker']
-handler.command = ['toimg']
+import { spawn } from 'child_process'
+import { format } from 'util'
 
-module.exports = handler
+let handler = async (m, { conn, usedPrefix, command }) => {
+    if (!global.support.convert &&
+        !global.support.magick &&
+        !global.support.gm) return handler.disabled = true // Disable if doesnt support
+    const notStickerMessage = `Reply sticker with command *${usedPrefix + command}*`
+    if (!m.quoted) throw notStickerMessage
+    let q = m.quoted
+    if (/sticker/.test(q.mediaType)) {
+        let sticker = await q.download()
+        if (!sticker) throw sticker
+        let bufs = []
+        const [_spawnprocess, ..._spawnargs] = [...(global.support.gm ? ['gm'] : global.support.magick ? ['magick'] : []), 'convert', 'webp:-', 'png:-']
+        let im = spawn(_spawnprocess, _spawnargs)
+        im.on('error', e => m.reply(format(e)))
+        im.stdout.on('data', chunk => bufs.push(chunk))
+        im.stdin.write(sticker)
+        im.stdin.end()
+        im.on('exit', () => {
+            conn.sendFile(m.chat, Buffer.concat(bufs), 'image.png', author, m)
+        })
+    } else throw notStickerMessage
+}
+handler.help = ['toimg (reply)']
+handler.tags = ['sticker']
+handler.command = /^toimg$/i
+
+export default handler
