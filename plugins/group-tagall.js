40566a387d0e9bdadf191ab2ef8bce5ff5cc477d
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-09-06 22:09:46 +0700

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants }) => {
 	let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`${text ? `${text}\n\n` : ''}` + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, { mentions: users })
+    m.reply(`${text ? `${text}\n\n` : ''}` + users.map(v => '@༢࿔ྀ' + v.replace(/@.+/, '')).join`\n`, null, { mentions: users })
 }
 
 handler.help = ['tagall']
