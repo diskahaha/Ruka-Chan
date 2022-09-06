98f8a675853fb6ddb9a282932e6223046fab98cf
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-09-06 18:31:56 +0700

    Update sticker-togif.js

--- plugins/sticker-togif.js
+++ plugins/sticker-togif.js
@@ -1,20 +1,17 @@
-import { webp2mp4 } from '../lib/webp2mp4.js'
-
+let { webp2mp4 } = require('../lib/webp2mp4')
 let handler = async (m, { conn, usedPrefix, command }) => {
-if (!m.quoted) throw `balas stiker dengan caption *${usedPrefix + command}*`
-   let q = m.quoted ? m.quoted : m
-   let mime = (q.msg || q).mimetype || ''
-    if (!/webp/g.test(mime)) throw `balas stiker dengan caption *${usedPrefix + command}*`
-    let media = await q.download?.()
+    if (!m.quoted) throw `balas stiker dengan perintah *${usedPrefix + command}*`
+    let mime = m.quoted.mimetype || ''
+    if (!/webp/.test(mime)) throw `balas stiker dengan perintah *${usedPrefix + command}*`
+    let media = await m.quoted.download()
     let out = Buffer.alloc(0)
-    if (/webp/g.test(mime)) {
+    if (/webp/.test(mime)) {
         out = await webp2mp4(media)
     }
-    conn.sendFile(m.chat, out, 'out.gif', m, false, { mimetype: 'video/gif', thumbnail: Buffer.alloc(0) })
+    await conn.sendFile(m.chat, out, 'out.gif', '*Nih Udah Jadii*', m, 0, { mimetype: 'video/gif', thumbnail: Buffer.alloc(0) })
 }
-
-handler.help = ['togif (reply media)', 'tovideo <reply>']
+handler.help = ['togif']
 handler.tags = ['sticker']
-handler.command = /^(togif|tovideo)$/i
+handler.command = ['togif']
 
-export default handler
+module.exports = handler
